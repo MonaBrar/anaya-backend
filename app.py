40c48dcdac94867b181c5392af1239b47c7cebb2
MonaBrar <mona.brar77@gmail.com>
2025-03-15 11:57:59 -0700

    Initial commit - cleaned up project structure

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Anaya: Your Personal Guide")
+st.write("Welcome to Anaya's learning system. Let's begin!")
+
+if st.button("Click Me"):
+    st.write("Hello, Monika! Anaya is listening. 💛✨")
